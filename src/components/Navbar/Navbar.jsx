db58f498f8b2113c7efb0c8c8afcc7c6063014b6
M <m4mohaiman@gmail.com>
2023-06-10 15:05:09 +0600

    header navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React from "react";
+import { styled } from "styled-components";
+import { Search, ShoppingCart } from "@material-ui/icons";
+import { Badge } from '@material-ui/core';
+
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: 0;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+  text-align: center;
+  text-transform: uppercase;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>ENG</Language>
+          <SearchContainer>
+            <Input></Input>
+            <Search style={{color:"grey", fontSize:16}}></Search>
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>E-Commerce.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Registration</MenuItem>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCart></ShoppingCart>
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import Announcement from '../../components/Announcement/Announcement'
 
 const Homepage = () => {
   return (
     <div>
+        <Announcement></Announcement>
         <Navbar></Navbar>
-        Homepage
     </div>
   )
 }

5f1fe4b4129f1cf39b9aa330a5dc8fcd564b7183
M <m4mohaiman@gmail.com>
2023-06-14 09:02:51 +0600

    product area added

--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../components/Navbar/Navbar'
 import Announcement from '../../components/Announcement/Announcement'
 import Slider from '../../components/Slider/Slider'
 import Categories from '../../components/Categories/Categories'
+import Products from '../../components/Products/Products'
 
 const Homepage = () => {
   return (
@@ -11,7 +12,7 @@ const Homepage = () => {
         <Navbar></Navbar>
         <Slider></Slider>
         <Categories></Categories>
-        
+        <Products></Products>
     </div>
   )
 }

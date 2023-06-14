ba7f71ed51bb153b0aac82f78b5abb3c65a59c20
batman <mohaiman.abdul@intellier.com>
2023-06-14 13:07:09 +0600

    cart added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@
 //import Product from "./pages/Product/Product";
 //import ProductList from "./pages/ProductList/ProductList";
 
-import Login from "./pages/Login/Login";
+import Cart from "./pages/Cart/Cart";
+//import Login from "./pages/Login/Login";
 //import Registration from "./pages/Registration/Registration";
 
 const App = () => {
   return(
     <>
-      <Login></Login>
+      <Cart></Cart>
     </>
   )
 };

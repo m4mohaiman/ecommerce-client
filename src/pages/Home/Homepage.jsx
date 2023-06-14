f056cdd6ce30de9943265a3d96a9365566d30039
batman <mohaiman.abdul@intellier.com>
2023-06-14 10:41:43 +0600

    newsletter component add

--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -4,6 +4,7 @@ import Announcement from '../../components/Announcement/Announcement'
 import Slider from '../../components/Slider/Slider'
 import Categories from '../../components/Categories/Categories'
 import Products from '../../components/Products/Products'
+import Newsletter from '../../components/Newsletter/Newsletter'
 
 const Homepage = () => {
   return (
@@ -13,6 +14,7 @@ const Homepage = () => {
         <Slider></Slider>
         <Categories></Categories>
         <Products></Products>
+        <Newsletter></Newsletter>
     </div>
   )
 }

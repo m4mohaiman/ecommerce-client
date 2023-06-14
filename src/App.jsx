bfecfdf040b9a340b4d31569c8d27318f0b2c215
batman <mohaiman.abdul@intellier.com>
2023-06-14 13:01:46 +0600

    login and resgistration page add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 //import Homepage from "./pages/Home/Homepage";
-import Product from "./pages/Product/Product";
+//import Product from "./pages/Product/Product";
 //import ProductList from "./pages/ProductList/ProductList";
+
+import Login from "./pages/Login/Login";
+//import Registration from "./pages/Registration/Registration";
 
 const App = () => {
   return(
     <>
-      <Product></Product>
+      <Login></Login>
     </>
   )
 };

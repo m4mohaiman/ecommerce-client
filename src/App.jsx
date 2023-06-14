6ba6c91535b1f60cba21ebb2e6675e229346b694
batman <mohaiman.abdul@intellier.com>
2023-06-14 11:22:13 +0600

    product list page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Homepage from "./pages/Home/Homepage";
+//import Homepage from "./pages/Home/Homepage";
+import Product from "./pages/Product/Product";
+//import ProductList from "./pages/ProductList/ProductList";
 
 const App = () => {
   return(
     <>
-      <Homepage></Homepage>
+      <Product></Product>
     </>
   )
 };

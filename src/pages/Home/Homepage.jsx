e979e2447bf561efea434b5ca98903f6d9b40fa0
M <m4mohaiman@gmail.com>
2023-08-18 00:22:49 +0600

    new letter and footer

--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -4,6 +4,9 @@ import Announcement from '../../components/Announcement/Announcement'
 import Slider from '../../components/Slider/Slider'
 import Categories from '../../components/Categories/Categories'
 import Products from '../../components/Products/Products'
+import Newsletter from '../../components/Newsletter/Newsletter'
+import Footer from '../../components/Footer/Footer'
+
 
 const Homepage = () => {
   return (
@@ -13,6 +16,8 @@ const Homepage = () => {
         <Slider></Slider>
         <Categories></Categories>
         <Products></Products>
+        <Newsletter></Newsletter>
+        <Footer></Footer>
     </div>
   )
 }
